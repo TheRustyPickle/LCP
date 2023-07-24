66417266d86b775ae12d88ab165911c0168f538e
Rusty Pickle <mohammad.sakib0194@gmail.com>
2023-07-25 04:50:40 +0600

    Update

--- src/main.rs
+++ src/main.rs
@@ -9,23 +9,5 @@ fn main() {
 pub struct Solution {}
 
 impl Solution {
-    /// magic math algo solution
-    pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
-        let n = nums.len();
-        let mut output = vec![1; n];
-        
-        let mut left_product = 1;
-        for i in 0..n {
-            output[i] *= left_product;
-            left_product *= nums[i];
-        }
-        
-        let mut right_product = 1;
-        for i in (0..n).rev() {
-            output[i] *= right_product;
-            right_product *= nums[i];
-        }
-        
-        output
-    }
+    
 }
